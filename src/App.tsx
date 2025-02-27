35690b497a814622d48570e5a0c7b436b639c557
Girish <pgirisha77@gmail.com>
2025-02-28 05:02:17 +0530

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className='flex flex-col w-full h-full items-center justify-center bg-black'>
-        <p className='text-3xl font-bold hover:text-6xl text-white'>Girishhh</p>
+        <p className='text-3xl font-bold hover:text-6xl text-white'>Hi, Girishhh</p>
       </div>
     </>
   )
