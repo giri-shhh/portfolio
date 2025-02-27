d1c99b8d13d3a5f83d2323e53f999f95307e417f
Girish <pgirisha77@gmail.com>
2025-02-28 05:00:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col w-full h-full items-center justify-center bg-black'>
+        <p className='text-3xl font-bold hover:text-6xl text-white'>Girishhh</p>
+      </div>
+    </>
+  )
+}
+
+export default App
